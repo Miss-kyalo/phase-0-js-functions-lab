9731c2bf1cf03b88ac23e8b8ebc4e39ad2c4acda
Miss-kyalo <bellakyalo@gmail.com>
2026-06-18 23:29:08 +0300

    functions

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-
-
-
+//function calculatetax
+function calculateTax( amount) {return amount *0.10;
+}
+//function converToUpperCase
+function convertToUpperCase( text) {return text.toUpperCase():
+}
+//findMaximum
+function findMaximum( num1, num2) {if (num1 > num2){return num1;} else {return num2;}
+}
+//isPalindrome
+isPalindrome( word) { String reversedWord = new StringBuilder(word).reverse().toString();
+if word.equals(reversedWord) {
+    return true;
+} else {
+    return false;
+}
+}
+//calculateDiscountedPrice
+function calculateDiscountedPrice( originalPrice, discountPercentage) {
+    let discountedPrice = originalPrice * (discountPercentage / 100);
+    return originalPrice - discountedPrice;
+}
 
 
 // This is required for the test to function properly  
